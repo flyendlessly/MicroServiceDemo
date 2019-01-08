6587b0287166b758d4d76945ae9e72c7335d32f6
even.wang <even.wang@sh-ad.chinascopefinancial.com>
2019-01-08 14:55:37 +0800

    添加项目文件。

---
+++ SoilHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoilHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
