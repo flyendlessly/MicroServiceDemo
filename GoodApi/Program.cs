208b66fd415b1dafed09ad440c65f67ae953f6c7
even.wang <even.wang@CSF0046>
2019-01-16 17:42:04 +0800

    Consul负载均衡+Ocelot负载均衡

--- GoodApi/Program.cs
+++ GoodApi/Program.cs
@@ -19,7 +19,7 @@ namespace GoodApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:10003")
+                .UseUrls("http://*:10003")
                 .UseStartup<Startup>();
     }
 }
