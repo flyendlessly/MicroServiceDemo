62dfcc619ac0b518d8ae2439f6a0a9d98ff07a09
WangYunfei <4eu_drm2j6p3j@dingtalk.com>
2019-11-20 09:45:40 +0800

    grpcserver

---
+++ GrpcServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
